7a494be8fffe8370065bb222ad0b338aac9e5afb
pranshul gupta <pranshulg01@gmail.com>
2025-01-20 01:19:32 +0530

    creating landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ImageToText from './components/ImageToText';
+import LandingPage from './components/LandingPage'
 
 const App = () => {
   return (
     <div>
       <h1>Image to Text Converter</h1>
       <ImageToText />
+      <LandingPage/>
     </div>
   );
 };
